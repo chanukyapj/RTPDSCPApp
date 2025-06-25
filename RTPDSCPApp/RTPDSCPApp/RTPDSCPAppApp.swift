daf58c30fb0e9735d22ee9a1128ed353586a7240
Chanukya PJ <cpj@avaya.com>
2025-06-25 23:15:21 +0530

    Added logger capture for MDM SDK

--- RTPDSCPApp/RTPDSCPApp/RTPDSCPAppApp.swift
+++ RTPDSCPApp/RTPDSCPApp/RTPDSCPAppApp.swift
@@ -11,39 +11,39 @@ import AWSDK
 
 @main
 struct RTPDSCPAppApp: App {
-    @StateObject private var viewModel = MyViewModel()
-    @State var showingAlert = false
-    
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	@StateObject private var viewModel = MyViewModel()
+	@State var showingAlert = false
+	
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
 
 
 class MyViewModel: AWControllerDelegate, ObservableObject {
-    init() {
-        initialisedAWSDK()
-    }
-    
-    func initialisedAWSDK () {
-        let awcontroller = AWController.clientInstance()
-        awcontroller.callbackScheme = "airwatchCallbackSchemeTest"
-        awcontroller.delegate = self
-        awcontroller.start()
-    }
-    
-    func controllerDidFinishInitialCheck(error: NSError?) {        
-        if (error != nil) {
-            print("AWSDK initialised failed:")
-            print("Domain: \(error!.domain)")
-            print("Code: \(error!.code)")
-            print("Description: \(error!.localizedDescription)")
-        }else {
-            print("AWSDK initialised success")
-        }
-    }
+	init() {
+		initialisedAWSDK()
+	}
+	
+	func initialisedAWSDK () {
+		let awcontroller = AWController.clientInstance()
+		awcontroller.callbackScheme = "airwatchCallbackSchemeTest"
+		awcontroller.delegate = self
+		awcontroller.start()
+	}
+	
+	func controllerDidFinishInitialCheck(error: NSError?) {
+		if (error != nil) {
+			FileLogger.shared.log("AWSDK initialised failed:")
+			FileLogger.shared.log("Domain: \(error!.domain)")
+			FileLogger.shared.log("Code: \(error!.code)")
+			FileLogger.shared.log("Description: \(error!.localizedDescription)")
+		}else {
+			FileLogger.shared.log("AWSDK initialised success")
+		}
+	}
 }
 
 
